f8b2baa412194b155cae9ac570131bd177799a1f
friday27 <yiyinghahalin@gmail.com>
2020-01-15 22:28:56 +0800

    course 46 ~ 49

--- weather-app/README.md
+++ weather-app/README.md
@@ -87,3 +87,27 @@
             console.log('Server is up on port 3000.');
         });
 
+* Static style
+
+        //import html files under public/
+        const path = require('path');
+        const publicDir = path.join(__dirname, '../public');
+        app.use(express.static(publicDir));
+
+* Dynamic style - hbs and render
+
+        //npm i hbs
+        app.set('view engine', 'hbs');
+
+        //create an index.hbs file under views/
+        app.get('', (req, res) => {
+            //render one of the views
+            // param1: name of the view
+            // param2: data obj which can be fetched by .hbs
+            res.render('index', { 
+                title: 'Weather App',
+                name: 'Lily Potter'
+            }); 
+        });
+
+
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,33 +1,51 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 // console.log(__dirname); //path of the current script
 // console.log(__filename); //path to this file
 
 // Create an Express application
 const app = express();
-const publicDir = path.join(__dirname, '../public');
 
+// Define paths for Express config
+const publicDir = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location
+// http://expressjs.com/en/4x/api.html#app.set
+app.set('view engine', 'hbs'); //handlebar extension
+app.set('views', viewsPath);
+hbs.registerPartials(partialPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDir));
 
-// replace by public/index.html
-// app.get('', (req, res) => {
-//     res.send('<h1>Weather</h1>');
-// });
+app.get('', (req, res) => {
+    //render one of the views
+    // param1: name of the view
+    // param2: data obj which can be fetched by .hbs
+    res.render('index', { 
+        title: 'Weather App',
+        name: 'Lily Potter'
+    }); 
+});
 
-// app.get('/help', (req, res) => {
-//     // Express will automatically convert object to JSON
-//     res.send([{
-//         name: 'Andrew',
-//         age: 18
-//     }, {
-//         name: 'Tom'
-//     }]);
-// });
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Lily Potter'
+    });
+});
 
-// app.get('/about', (req, res) => {
-//     res.send('<h1>About</h1>');
-// });
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help!',
+        name: 'Harry Potter',
+        helpMsg: 'I am here to help :)'
+    });
+});
 
 app.get('/weather', (req, res) => {
     res.send({
